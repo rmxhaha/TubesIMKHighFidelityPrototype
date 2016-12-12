134a978fb764c21feaa9612e4576dea162c6f7d2
rmxhaha <code000123@gmail.com>
2016-12-12 19:20:19 +0700

    move to angular route way

--- js/groop.js
+++ js/groop.js
@@ -1,32 +1,62 @@
-angular.module('groop-app', [])
-  .controller('main-search',['$scope', function($scope) {
-      $scope.type = "namakomunitas";
-  }])
-  .controller("group-popular-discussion",["$scope",function($scope){
-    $scope.posts = [
-      {
-        upvote : 121,
-        poster_img : 'img/gambar.jpg',
-        title : 'Rakit Komputer 2jt',
-        content : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id nisi id ipsum auctor tempor a nec felis.',
-        comment_count : 30,
-        post_url : 'discussion2.html'
-      },
-      {
-        upvote : 121,
-        poster_img : 'img/gambar.jpg',
-        title : 'Hibah Komputer',
-        content : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id nisi id ipsum auctor tempor a nec felis.',
-        comment_count : 30,
-        post_url : 'discussion2.html'
-      },
-      {
-        upvote : 121,
-        poster_img : 'img/gambar.jpg',
-        title : 'Rakit Komputer 2jt',
-        content : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id nisi id ipsum auctor tempor a nec felis.',
-        comment_count : 30,
-        post_url : 'discussion2.html'
-      },
-    ];
-  }]);
+var groop = angular.module('groop-app', ['ngRoute']);
+
+groop.controller('nav',['$scope','$rootScope',function($scope, $rootScope){
+  $rootScope.$on('user_login', function(user){
+    $scope.logged_in = true;
+  });
+
+}]);
+
+groop.config(function($routeProvider) {
+     $routeProvider
+         // route for the home page
+         .when('/', {
+             templateUrl : 'pages/main-search.html',
+             controller  : 'main-search'
+         })
+
+         // route for the about page
+         .when('/search', {
+             templateUrl : 'pages/main-search-result.html',
+             controller  : 'aboutController'
+         })
+
+         // route for the contact page
+         .when('/contact', {
+             templateUrl : 'pages/contact.html',
+             controller  : 'contactController'
+         });
+ });
+
+groop.controller('main-search',['$scope', function($scope) {
+  $scope.type = "namakomunitas";
+}]);
+
+groop.controller("home-profile",["$scope",function($scope){
+  $scope.posts = [
+    {
+      upvote : 121,
+      poster_img : 'img/gambar.jpg',
+      title : 'Rakit Komputer 2jt',
+      content : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id nisi id ipsum auctor tempor a nec felis.',
+      comment_count : 30,
+      post_url : 'discussion2.html'
+    },
+    {
+      upvote : 121,
+      poster_img : 'img/gambar.jpg',
+      title : 'Hibah Komputer',
+      content : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id nisi id ipsum auctor tempor a nec felis.',
+      comment_count : 30,
+      post_url : 'discussion2.html'
+    },
+    {
+      upvote : 121,
+      poster_img : 'img/gambar.jpg',
+      title : 'Rakit Komputer 2jt',
+      content : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id nisi id ipsum auctor tempor a nec felis.',
+      comment_count : 30,
+      post_url : 'discussion2.html'
+    },
+  ];
+}]);
